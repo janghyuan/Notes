e8d917216209e10e7cc30cf69e4b83eb5f3c3d6d
janghyuan <janghyuan@163.com>
2016-04-17 08:24:20 +0800

    add divide and conquer method

--- Algorithms/max_sub_array.rb
+++ Algorithms/max_sub_array.rb
@@ -33,5 +33,32 @@ def max_sub_array(arr)
 	max_sum
 end
 
+# divide and conquer
+def max_sub_array(arr, from, to)
+	if from == to
+		return arr[from]
+	end
+
+	middle = (from + to) / 2
+	m1 = max_sub_array(arr, from, middle)
+	m2 = max_sub_array(arr, middle + 1, to)
+
+	left = arr[middle]
+	now = arr[middle]
+	arr[from..(middle-1)].reverse_each do |val|
+		now += val
+		left = now if now > left
+	end
+
+	right = arr[middle + 1]
+	now = arr[middle + 1]
+	arr[(middle+2)..to].each do |val|
+		now += val
+		right = now if now > right
+	end
+
+	m1 > m2 ? ( m1 > m3 ? m1 : m3) : ( m2 > m3 ? m2 : m3)
+end
+
 arr = [-1, 2, 3, -5, 9]
 puts max_sub_array(arr)
