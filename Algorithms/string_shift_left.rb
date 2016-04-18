c8187374a31c0cc340669be0838ae68b2dadc2de
janghyuan <janghyuan@163.com>
2016-04-18 14:05:12 +0800

    add string shift left

---
+++ Algorithms/string_shift_left.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+#
+# 字符串循环左移
+#
+# 要求：空间复杂度O(1), 时间复杂度O(n)
+# 公式：(A'B')' == BA
+#
+# 因为字符串的翻转操作是 空间是O(1) ，时间是O(n) 
+
+def shift_left(string, n)
+	(string[0..(n-1)].reverse + string[n..(string.length-1)].reverse).reverse
+end
+
+puts shift_left("abcdef", 2) #=>cdefab
